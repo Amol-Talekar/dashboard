0ebfe2d7471fe7eff66fd5f0846beaf5dcb7e87c
Amol-Talekar <amol11talekar@gmail.com>
2023-08-26 01:33:01 +0530

    feature: Added Navbar to the client that contains searchbar and mode toggler

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import "./App.css";
+// import "./App.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
 import { themeSettings } from "theme";
 
 function App() {
@@ -11,9 +14,17 @@ function App() {
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
